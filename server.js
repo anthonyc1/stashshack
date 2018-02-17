386c3392d626dafa0f0b22cfab1e6f03e4a65bd3
Anthony Chan <anthonychan0123@gmail.com>
2018-02-17 17:48:17 -0500

    develop pages

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
+app.get('/raffle', function (req, res) {
+	res.render('entry');
+});
+
+app.post('/raffle', function (req, res) {
+	const number = req.body.number;
+	if (number >= 0 && number <= 1000){
+		res.render('submitted', {number: number});
+	} else{
+		res.render('error', {number: number});
+	}
+});
+
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
 })
