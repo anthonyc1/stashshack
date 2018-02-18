96ff678fb9251eff5d6dd13b10fcec993c84d73d
Anthony Chan <anthonychan0123@gmail.com>
2018-02-17 23:07:37 -0500

    getting mongodb to work with node

--- routes/verifyNumber.js
+++ routes/verifyNumber.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const monk = require('monk');
+
+var db = monk('localhost:27017/sms');
+var numbers = db.get('numbers');
 
 const path = require('path');
 var router = express.Router();
+
+var code;
+
+function generateCode(){
+	var num = Math.floor(Math.random()*10000);
+	if (num < 1000){
+		num *= 10;
+	}
+	return num;
+}
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
@@ -13,16 +27,15 @@ router.get('/home', function (req, res) {
 });
 
 router.post('/number', function (req, res) {
+	var number = req.body.number;
+	code = generateCode();
+	numbers.insert({ 'number' : number, 'code' : code});
 	res.render('verify');
 });
 
-// router.post('/verify', function (req, res) {
-// 	res.render('index');
-// });
-
 router.post('/verify', function (req, res) {
-	var number = req.body.code;
-	
+	var code = req.body.code;
+	obj = numbers.find({ 'number' : number});
 });
 
 module.exports = router;
